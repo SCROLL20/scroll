538765324f33a404aa1451d2a81232d1dcdf6f47
SCROLL20 <80055303+SCROLL20@users.noreply.github.com>
2025-01-10 18:21:51 +0100

    Add files via upload

---
+++ DaikichiPathVariabls/src/main/java/com/example/DaikichiPathVariabls/DaikichiPathVariablsApplication.java
@@ -0,0 +1,13 @@
+package com.example.DaikichiPathVariabls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaikichiPathVariablsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaikichiPathVariablsApplication.class, args);
+	}
+
+}
---
+++ DaikichiPathVariabls/src/test/java/com/example/DaikichiPathVariabls/DaikichiPathVariablsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DaikichiPathVariabls;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaikichiPathVariablsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
