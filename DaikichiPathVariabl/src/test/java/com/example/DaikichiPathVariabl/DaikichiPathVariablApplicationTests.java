573bb8f5125496f8c8a417a4f71418908b350e43
SCROLL20 <80055303+SCROLL20@users.noreply.github.com>
2025-01-11 12:51:37 +0100

    Add files via upload

---
+++ DaikichiPathVariabl/src/test/java/com/example/DaikichiPathVariabl/DaikichiPathVariablApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DaikichiPathVariabl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaikichiPathVariablApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
