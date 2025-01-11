e23eec4518b3c360494e2fd70245d04d4b787e52
SCROLL20 <80055303+SCROLL20@users.noreply.github.com>
2025-01-11 12:27:57 +0100

    Add files via upload

---
+++ HelloHumanApllication/src/main/java/com/example/HelloHumanApplication/HelloHumanApllicationApplication.java
@@ -0,0 +1,13 @@
+package com.example.HelloHumanApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloHumanApllicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloHumanApllicationApplication.class, args);
+	}
+
+}
---
+++ HelloHumanApllication/src/test/java/com/example/HelloHumanApplication/HelloHumanApllicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HelloHumanApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloHumanApllicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
