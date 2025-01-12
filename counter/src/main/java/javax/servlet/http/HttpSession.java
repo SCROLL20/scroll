6849100f4b2a5ffed431df04627a60fe34340047
SCROLL20 <80055303+SCROLL20@users.noreply.github.com>
2025-01-13 00:57:07 +0100

    Add files via upload

---
+++ counter/src/main/java/javax/servlet/http/HttpSession.java
@@ -0,0 +1,15 @@
+package javax.servlet.http;
+
+public class HttpSession {
+
+	public Integer getAttribute(String string) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void setAttribute(String string, Integer count) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
