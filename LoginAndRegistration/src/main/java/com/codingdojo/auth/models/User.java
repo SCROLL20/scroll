cde193080a3a77fdc462b0546451d9b4ef5b9e35
SCROLL20 <80055303+SCROLL20@users.noreply.github.com>
2025-04-25 19:54:55 +0200

    Add files via upload

---
+++ LoginAndRegistration/src/main/java/com/codingdojo/auth/models/User.java
@@ -0,0 +1,30 @@
+package com.codingdojo.auth.models;
+
+public class User {
+
+	public Object getId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getEmail() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object getConfirm() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getPassword() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void setPassword(String hashed) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
